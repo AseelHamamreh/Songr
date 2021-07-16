191b5e111e1637e8f31b647aeede67645ba51842
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-07-16 04:27:32 +0300

    initial commit from Spring init

---
+++ src/main/java/com/songrApp/songr/generalControllers.java
@@ -0,0 +1,10 @@
+package com.songrApp.songr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class generalControllers {
+
+}
